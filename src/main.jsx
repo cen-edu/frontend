190815020d403103f4cefa4e69e3fda7c312224d
gorani77 <sp2877@knou.ac.kr>
2026-07-30 09:47:01 +0900

    feat : 전역 폰트 세팅 - 프리텐다드

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
-import '../index.css';
+import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
