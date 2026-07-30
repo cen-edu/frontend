00aa0cff0f3e46e19a019798cb413b966550c0ba
gorani77 <sp2877@knou.ac.kr>
2026-07-30 10:03:17 +0900

    feat : 로그인 페이지 와이어프레임 작성

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css'
 import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
