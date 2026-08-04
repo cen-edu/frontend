6ae241e3710d0405a835ceaf33ff237dc274138b
gorani77 <sp2877@knou.ac.kr>
2026-08-04 11:45:58 +0900

    feat : 헤더랑 사이드 스타일 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css'
+import '@kfonts/hakgyoansim-bunpil'
 import './index.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
